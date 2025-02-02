b3284e6a00aa6cd7abffa3ae66394222184e8709
Jakob Hellermann <jakob.hellermann@protonmail.com>
2025-02-02 17:13:51 +0100

    options: `clear_after_hunk`

--- src/diff_patch.rs
+++ src/diff_patch.rs
@@ -4,6 +4,7 @@ use std::path::Path;
 use anyhow::Result;
 use diffy::PatchFormatter;
 use nu_ansi_term::{Color, Style};
+use termion::cursor::DetectCursorPos;
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::{IntoRawMode, RawTerminal};
@@ -11,12 +12,14 @@ use termion::raw::{IntoRawMode, RawTerminal};
 use crate::changes::Changes;
 
 pub struct Options {
+    clear_after_hunk: bool,
     immediate_command: bool,
 }
 
 impl Default for Options {
     fn default() -> Self {
         Self {
+            clear_after_hunk: false,
             immediate_command: true,
         }
     }
@@ -61,35 +64,86 @@ impl DiffPatch {
         modified_path: &Path,
         modified_content: &str,
     ) -> Result<()> {
-        let mut options = diffy::DiffOptions::new();
+        let size = termion::terminal_size()?;
 
-        let original_path_str = original_path.display().to_string();
-        let modified_path_string = modified_path.display().to_string();
-        options.set_original_filename(original_path_str);
-        options.set_modified_filename(modified_path_string);
+        let mut diff_options = diffy::DiffOptions::new();
+        diff_options.set_original_filename(original_path.display().to_string());
+        diff_options.set_modified_filename(modified_path.display().to_string());
 
-        let patch = options.create_patch(&original_content, &modified_content);
+        let patch = diff_options.create_patch(&original_content, &modified_content);
 
         let n_hunks = patch.hunks().len();
 
-        write_header(
-            std::io::stdout().lock(),
-            Some(&original_path),
-            Some(&modified_path),
-        )?;
+        let mut writer = CountLines::new(std::io::stdout().lock());
+        write_header(&mut writer, Some(&original_path), Some(&modified_path))?;
+        let header_len = writer.take_lineno();
 
         for (i, hunk) in patch.hunks().iter().enumerate() {
-            self.formatter
-                .write_hunk_into(hunk, &mut self.stdout.lock())?;
+            self.formatter.write_hunk_into(hunk, &mut writer)?;
 
             self.ask(&format!(
                 "({}/{}) Stage this hunk [y,n,q,a,d,e]? ",
                 i + 1,
                 n_hunks
             ))?;
+
+            if self.options.clear_after_hunk {
+                let hunk_len = writer.take_lineno();
+                let last = i == n_hunks - 1;
+                let erase = match last {
+                    true => header_len + hunk_len,
+                    false => hunk_len,
+                };
+
+                self.stdout.activate_raw_mode()?;
+                let new_pos = self.stdout.cursor_pos()?;
+                self.stdout.suspend_raw_mode()?;
+
+                let lines = erase + 1;
+                let extra = (new_pos.1 == size.1) as u16;
+                self.erase_last_lines(lines + extra)?;
+            }
         }
 
         Ok(())
+    }
+
+    fn erase_last_lines(&mut self, n: u16) -> Result<(u16, u16)> {
+        self.stdout.activate_raw_mode()?;
+        let pos = self.stdout.cursor_pos()?;
+        self.stdout.suspend_raw_mode()?;
+        eprintln!("{:?}", pos);
+
+        let new_pos = (pos.0, pos.1.saturating_sub(n));
+        write!(
+            self.stdout,
+            "{}{}",
+            termion::cursor::Goto(1, new_pos.1),
+            termion::clear::AfterCursor,
+        )?;
+        self.stdout.flush()?;
+
+        Ok(pos)
+    }
+}
+
+struct CountLines<W>(W, usize);
+impl<W> CountLines<W> {
+    fn new(w: W) -> Self {
+        CountLines(w, 0)
+    }
+    fn take_lineno(&mut self) -> u16 {
+        std::mem::take(&mut self.1) as u16
+    }
+}
+impl<W: std::io::Write> std::io::Write for CountLines<W> {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        self.1 += buf.iter().filter(|&&x| x == b'\n').count();
+        self.0.write(buf)
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        self.0.flush()
     }
 }
 
